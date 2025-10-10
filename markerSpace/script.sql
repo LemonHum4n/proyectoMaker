c8016a04badd78beba6af33bcc721d7dd9b4ace9
Arimika <s.mateocuevas@liceorbl.cl>
2025-10-10 12:07:31 -0300

    cambio base de datos

--- markerSpace/script.sql
+++ markerSpace/script.sql
@@ -40,17 +40,6 @@ CREATE TABLE IF NOT EXISTS `esquema_maker`.`users` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `esquema_maker`.`horario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `esquema_maker`.`horario`(
-	`id` time Not null,
-    `hora_reserva` int not null  auto_increment, 
-    `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	primary key(`id`)
-)
-	ENGINE = InnoDB;
 
 
 
@@ -60,41 +49,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema esquema_maker
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema esquema_maker
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `esquema_maker` DEFAULT CHARACTER SET utf8 ;
-USE `esquema_maker` ;
-
--- -----------------------------------------------------
--- Table `esquema_maker`.`classrooms`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `esquema_maker`.`zone` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `esquema_maker`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `esquema_maker`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `email` VARCHAR(100) NULL,
-  `password` VARCHAR(200) NULL,
-  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -133,8 +87,6 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-drop table reserva;
 
 
 
